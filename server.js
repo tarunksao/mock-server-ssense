f8641972bf720b7f664e36c159edae0544c64338
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2022-10-02 21:05:49 +0530

    Added mock data for  ssense

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server'); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('ssense.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
